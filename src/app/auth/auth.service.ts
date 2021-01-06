6aa0da4b6b208b42b695829fedae751c2a5d1cb9
Eightsh One <eightsh.one@gmail.com>
2021-01-06 22:05:55 +0100

    feat(services): add auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from  '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { tap } from  'rxjs/operators';
+import { Observable, BehaviorSubject } from  'rxjs';
+
+import { Storage } from  '@ionic/storage';
+import { User } from  './user';
+import { AuthResponse } from  './auth-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  AUTH_SERVER_ADDRESS:  string  =  'http://localhost:3000';
+  authSubject  =  new  BehaviorSubject(false);
+
+  constructor(private  httpClient:  HttpClient, private  storage:  Storage) { }
+
+  login(user: User): Observable<AuthResponse> {
+    return this.httpClient.post(`${this.AUTH_SERVER_ADDRESS}/users/login`, user).pipe(
+      tap(async (res: AuthResponse) => {
+
+        if (res.user) {
+          await this.storage.set("ACCESS_TOKEN", res.user.token);
+          this.authSubject.next(true);
+        }
+      })
+    );
+  }
+
+  async logout() {
+    await this.storage.remove("ACCESS_TOKEN");
+    this.authSubject.next(false);
+  }
+
+  isLoggedIn() {
+    return this.authSubject.asObservable();
+  }
+
+}
