d026c1de6d805f856d14f0c114e297c5c757ff4d
Eightsh One <eightsh.one@gmail.com>
2021-01-06 22:05:35 +0100

    fix(app comp): change tab selector behaviour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,9 +50,10 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
+    const path = window.location.pathname;
+    
     if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+      this.selectedIndex = this.appPages.findIndex(page => page.url.toLowerCase() === path.toLowerCase());
     }
   }
 }
