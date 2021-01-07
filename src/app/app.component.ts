68d94d00bc3bde4a5370a1547760db8c6d557ada
Mejdi Elkout <elkont.mejdi@gmail.com>
2021-01-07 23:11:43 +0100

    added logic for the menu's options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from './auth/auth.service';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -11,6 +11,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent implements OnInit {
   public selectedIndex = 0;
+  public menus = []
   public appPages = [
     {
       title: 'Acceuil',
@@ -33,13 +34,40 @@ export class AppComponent implements OnInit {
       icon: 'person'
     }
   ];
+  public connectedAppPages = [
+    {
+      title: 'Acceuil',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'Rechercher une formation',
+      url: '/courses',
+      icon: 'newspaper'
+    },
+    {
+      title: 'Mes formations',
+      url: '/training',
+      icon: 'albums'
+    },
+    {
+      title: 'Logout',
+      url: '/logout',
+      icon: 'log-out'
+    }
+  ];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private  authService:  AuthService
   ) {
     this.initializeApp();
+  }
+
+  async concatMenu(value) {
+
   }
 
   initializeApp() {
@@ -53,7 +81,14 @@ export class AppComponent implements OnInit {
     const path = window.location.pathname;
     
     if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.url.toLowerCase() === path.toLowerCase());
+      this.authService.isLoggedIn().subscribe(res => {
+        if (res) {
+          this.menus = this.connectedAppPages;
+        } else {
+          this.menus = this.appPages;
+        }
+        this.selectedIndex = this.menus.findIndex(page => page.url.toLowerCase() === path.toLowerCase());
+      });
     }
   }
 }
